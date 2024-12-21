e12b1663817df0e12adb4454f3692d3f39102342
aadityarohit <aadityarohit@umass.edu>
2024-12-21 13:23:38 -0500

    fixed upload rubric route

--- backend/app/models/request.py
+++ backend/app/models/request.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
-from fastapi import UploadFile
 
-
+class EmbedRubricRequest(BaseModel):
+    user_id: str
+    """The user ID of the namespace in which to embed the document"""
 class QueryRequest(BaseModel):
     query: str
+    user_id: str
     """Search query for retrieving relevant documents."""
--- backend/app/routes/embeddings.py
+++ backend/app/routes/embeddings.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter, UploadFile, File
-from models.request import QueryRequest
+from models.request import EmbedRubricRequest, QueryRequest
 from models.response import QueryResponse
 from services.embeddings import extract_text_from_pdf, embed_doc
 from db.pinecone_client import pc
 
 router = APIRouter()
 
-@router.post("/embeddoc")
-async def embed_doc(file: UploadFile = File(...)):
+@router.post("/upload-rubric")
+async def upload_and_store_doc(request: EmbedRubricRequest, file: UploadFile = File(...)):
     file_bytes = await file.read()
     extracted_text = extract_text_from_pdf(file_bytes)
-    doc_embeddings = embed_doc(extracted_text)
-    pc.upsert_vectors(doc_embeddings, "12345") # TODO: GET USER ID
+    doc_embeddings = embed_doc(extracted_text, file.filename)
+    pc.upsert_vectors(doc_embeddings, request.user_id)
+    return {"message" : "Rubric uploaded successfully"}
